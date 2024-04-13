43f1ecd202c7fb4d3ab389daf878f2e55f094ed1
Jahongirjon Xoliqov <j.xoliqov@corp.click.uz>
2024-04-13 09:29:31 +0500

    pie chart is on work

--- MyWallet/MonitoringViewController.swift
+++ MyWallet/MonitoringViewController.swift
@@ -8,175 +8,73 @@
 import UIKit
 
 final class MonitoringViewController: UIViewController {
-
-    struct PieSliceData {
-        var procent: Double
-    }
     
-    struct PieData {
-        var slices: [PieSliceData]
-    }
-    
-    struct PieSliceLayerData {
-        var startAngle: CGFloat
-        var endAngle: CGFloat
-        var centerPoint: CGPoint
-        var colors: [CGColor]
-    }
-    
-    class PieSliceLayer: CALayer {
-        
-        var data: PieSliceLayerData?
-        
-        private var shapeLayer = CAShapeLayer()
-        private var shadowLayer = CAShapeLayer()
-        private var gradientLayer = CAGradientLayer()
-        private var shapePath = UIBezierPath()
-        //private var shadowPath: UIBezierPath
-        
-        func draw(data: PieSliceLayerData) {
-            self.data = data
-            let rect = bounds
-            shapePath = UIBezierPath(arcCenter: CGPoint(x: rect.midX, y: rect.midY),
-                                     radius: rect.width/2,
-                                     startAngle: data.startAngle,
-                                     endAngle: data.endAngle,
-                                     clockwise: true)
-            
-            gradientLayer.type = .radial
-            gradientLayer.frame = bounds
-            gradientLayer.colors = data.colors
-            gradientLayer.startPoint = .init(x: 0.5, y: 0.5)
-            gradientLayer.endPoint = .init(x: 1, y: 1)
-            
-            shapeLayer.path = shapePath.cgPath
-            gradientLayer.mask = shapeLayer
-            
-            gradientLayer.removeFromSuperlayer()
-            addSublayer(gradientLayer)
-            
-        }
-        
-        
-        
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-    }
-
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        let pieRect = CGRect(x: view.frame.midX-100, y: view.frame.midY-100, width: 200, height: 200)
-        let pieChartLayer = CALayer()
-        pieChartLayer.frame = pieRect
-        view.layer.addSublayer(pieChartLayer)
-//        fetchPieData { pieData in
-//            
-//            let firstPieSliceLayerData = PieSliceLayerData(startAngle: 0,
-//                                                           endAngle: 288*CGFloat.pi/180,
-//                                                           centerPoint: CGPoint(x: pieRect.midX, y: pieRect.midY),
-//                                                           colors: [UIColor.systemOrange.cgColor, UIColor.systemPink.cgColor])
-//            let secondPieSliceLayerData = PieSliceLayerData(startAngle: 288*CGFloat.pi/180,
-//                                                           endAngle: 303*CGFloat.pi/180,
-//                                                           centerPoint: CGPoint(x: pieRect.midX, y: pieRect.midY),
-//                                                           colors: [UIColor.blue.cgColor, UIColor.cyan.cgColor])
-//            let thirdPieSliceLayerData = PieSliceLayerData(startAngle: 303*CGFloat.pi/180,
-//                                                           endAngle: 360*CGFloat.pi/180,
-//                                                           centerPoint: CGPoint(x: pieRect.midX, y: pieRect.midY),
-//                                                           colors: [UIColor.green.cgColor, UIColor.magenta.cgColor])
-//            
-//            
-//            let firstPieSliceLayer = PieSliceLayer()
-//            firstPieSliceLayer.frame = pieChartLayer.bounds
-//            firstPieSliceLayer.draw(data: firstPieSliceLayerData)
-//            pieChartLayer.addSublayer(firstPieSliceLayer)
-//            
-//            let secondPieSliceLayer = PieSliceLayer()
-//            secondPieSliceLayer.frame = pieChartLayer.bounds
-//            secondPieSliceLayer.draw(data: secondPieSliceLayerData)
-//            pieChartLayer.addSublayer(secondPieSliceLayer)
-//            
-//            let thirdPieSliceLayer = PieSliceLayer()
-//            thirdPieSliceLayer.frame = pieChartLayer.bounds
-//            thirdPieSliceLayer.draw(data: thirdPieSliceLayerData)
-//            pieChartLayer.addSublayer(thirdPieSliceLayer)
-//            
-//        }
         
-        let rect = CGRect(x: 0, y: 0, width: view.bounds.width-40, height: 100)
+        let centerRect = CGRect(x: view.frame.width/2-200, y: view.frame.height/2-200, width: 400, height: 400)
+        let pathCenterRect = CGRect(origin: .zero, size: CGSize(width: 400, height: 400))
+
+        let pi = CGFloat.pi
+        let radius: CGFloat = 50
         
         let path = UIBezierPath()
-        path.move(to: CGPoint(x: 100, y: 100))
-        path.addLine(to: CGPoint(x: 100, y: 0))
-        path.addArc(withCenter: CGPoint(x: 100, y: 100), radius: 100, startAngle: -CGFloat.pi/2, endAngle: CGFloat.pi, clockwise: true)
-        path.addLine(to: CGPoint(x: 100, y: 100))
-        path.close()
+
+        path.move(to: .init(x: 0, y: 300))
+        path.addLine(to: .init(x: 0, y: radius))
+        path.addQuadCurve(to: .init(x: radius, y: 0),
+                          controlPoint: .init(x: 0, y: 0))
+        path.addQuadCurve(to: .init(x: 300, y: 300-radius),
+                          controlPoint: .init(x: 300, y: 0))
+        path.addQuadCurve(to: .init(x: 300-radius, y: 300),
+                          controlPoint: .init(x: 300, y: 300))
+        path.addLine(to: .init(x: 0, y: 300))
         
-        let gradientLayer = CAGradientLayer()
-        gradientLayer.frame = CGRect(origin: CGPoint(x: 100, y: 100), size: CGSize(width: 200, height: 200))//CGRect(x: 20, y: view.bounds.midY-50, width: view.bounds.width-40, height: 100)
-        gradientLayer.colors = [UIColor.systemOrange.cgColor, UIColor.systemPink.cgColor]
-        gradientLayer.startPoint = .init(x: 0.5, y: 0)
-        gradientLayer.endPoint = .init(x: 0.5, y: 1)
         
         let shapeLayer = CAShapeLayer()
         shapeLayer.path = path.cgPath
+        shapeLayer.fillColor = UIColor.black.cgColor
+//        shapeLayer.strokeColor = UIColor.black.cgColor
+//        shapeLayer.strokeStart = 0
+//        shapeLayer.strokeEnd = 1
+//        shapeLayer.lineWidth = 10
+//        shapeLayer.lineCap = .butt
+//        shapeLayer.cornerRadius = 10
+        shapeLayer.shadowOffset = .zero
+        shapeLayer.shadowRadius = 10
+        shapeLayer.shadowOpacity = 1
+        shapeLayer.shadowColor = UIColor.black.cgColor
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = centerRect
+        gradientLayer.colors = [UIColor.yellow.cgColor, UIColor.orange.cgColor]
+        gradientLayer.startPoint = .init(x: 0.5, y: 0.5)
+        gradientLayer.endPoint = .init(x: 1, y: 1)
+        gradientLayer.type = .radial
+        
         gradientLayer.mask = shapeLayer
-        
-//        let shadowLayer = CAShapeLayer()
-//        shadowLayer.masksToBounds = false
-//        shadowLayer.shadowColor = UIColor.black.cgColor
-//        shadowLayer.shadowRadius = 16
-//        shadowLayer.shadowOffset = .zero
-//        shadowLayer.shadowOpacity = 1
-//        shadowLayer.path = UIBezierPath(ovalIn: CGRect(x: 20, y: view.bounds.midY-50, width: view.bounds.width-40, height: 100)).cgPath
-//        shadowLayer.fillColor = UIColor.red.cgColor
-//        shadowLayer.strokeColor = UIColor.clear.cgColor
-        
-//        view.layer.addSublayer(shadowLayer)
         view.layer.addSublayer(gradientLayer)
         
-    }
-    
-    private func fetchPieData(completion: @escaping (PieData) -> ()) {
-        let firstSlice = PieSliceData(procent: 80)
-        let secondSlice = PieSliceData(procent: 15)
-        let thirdSlice = PieSliceData(procent: 5)
-        let pieData = PieData(slices: [firstSlice, secondSlice, thirdSlice])
-        completion(pieData)
+        let shadowLayer = CAShapeLayer()
+        shadowLayer.path = path.cgPath
+        shadowLayer.fillColor = nil
+        shadowLayer.strokeColor = UIColor.black.cgColor
+        shadowLayer.strokeStart = 0
+        shadowLayer.strokeEnd = 1
+        shadowLayer.lineWidth = 2
+        shadowLayer.shadowOffset = .zero
+        shadowLayer.shadowRadius = 10
+        shadowLayer.shadowOpacity = 1
+        shadowLayer.shadowColor = UIColor.black.cgColor
+        
     }
     
 }
 
 
-/*
- let rect = CGRect(x: 0, y: 0, width: view.bounds.width-40, height: 100)
- 
- let path = UIBezierPath(ovalIn: rect)
- 
- let gradientLayer = CAGradientLayer()
- gradientLayer.frame = CGRect(x: 20, y: view.bounds.midY-50, width: view.bounds.width-40, height: 100)
- gradientLayer.colors = [UIColor.systemOrange.cgColor, UIColor.systemPink.cgColor]
- gradientLayer.startPoint = .init(x: 0.5, y: 0)
- gradientLayer.endPoint = .init(x: 0.5, y: 1)
- 
- let shapeLayer = CAShapeLayer()
- shapeLayer.path = path.cgPath
- gradientLayer.mask = shapeLayer
- 
- let shadowLayer = CAShapeLayer()
- shadowLayer.masksToBounds = false
- shadowLayer.shadowColor = UIColor.black.cgColor
- shadowLayer.shadowRadius = 16
- shadowLayer.shadowOffset = .zero
- shadowLayer.shadowOpacity = 1
- shadowLayer.path = UIBezierPath(ovalIn: CGRect(x: 20, y: view.bounds.midY-50, width: view.bounds.width-40, height: 100)).cgPath
- shadowLayer.fillColor = UIColor.red.cgColor
- shadowLayer.strokeColor = UIColor.clear.cgColor
- 
- view.layer.addSublayer(shadowLayer)
- view.layer.addSublayer(gradientLayer)
- 
- //view.layer.addSublayer(gradientLayer)
- */
+
+extension CGRect {
+    var center: CGPoint {
+        CGPoint(x: midX, y: midY)
+    }
+}
